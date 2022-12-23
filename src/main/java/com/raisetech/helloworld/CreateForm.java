43a8ae4f61131d9fd766d466937cae9d40871b4b
NakagawaYuuto <github.nakagawa@gmail.com>
2022-12-23 22:42:56 +0900

    frist commit

---
+++ src/main/java/com/raisetech/helloworld/CreateForm.java
@@ -0,0 +1,14 @@
+package com.raisetech.helloworld;
+
+public class CreateForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/raisetech/helloworld/NameController.java
@@ -0,0 +1,32 @@
+package com.raisetech.helloworld;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class NameController {
+
+    @GetMapping("/names")
+    public List<String> getName(){
+        return List.of("nakagawa", "nakazima");
+    }
+
+    @PostMapping("/names")
+    public ResponseEntity<String> createName(@RequestBody CreateForm form){
+        URI url = UriComponentsBuilder.fromUriString("http://localhost:8080")
+                .path("/names/id")
+                .build()
+                .toUri();
+        return ResponseEntity.created(url).body("name successfully created");
+    }
+
+    @PatchMapping("/names/{id}")
+    public ResponseEntity<Map<String, String >> updateName(@PathVariable("id")int id, @RequestBody UpdateForm from) {
+        return ResponseEntity.ok(Map.of("message", "name successfully updated"));
+    }
+}
